8c2f250086868d169ceda6c8ee588dbd548aa8f8
gadi9889 <gadi8998@gmail.com>
2024-09-11 18:53:20 +0300

    Complete simple calc interpreter

--- main.py
+++ main.py
@@ -2,30 +2,20 @@ INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
     'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF'
 )
 
-
-class Token(object):
+class Token:
     def __init__(self, type: str, value: any) -> None:
         self.type = type
         self.value = value
 
     def __str__(self) -> str:
-        """String representation of the class instance.
-
-        Examples:
-            Token(INTEGER, 3)
-            Token(PLUS, '+')
-            Token(MUL, '*')
-        """
         return 'Token({type}, {value})'.format(
-            type=self.type,
-            value=repr(self.value)
+            type=self.type, value=repr(self.value)
         )
 
     def __repr__(self) -> str:
         return self.__str__()
 
-
-class Lexer(object):
+class Lexer:
     def __init__(self, text: str) -> None:
         self.text = text
         self.pos = 0
@@ -35,19 +25,14 @@ class Lexer(object):
         raise Exception('Invalid character')
 
     def advance(self) -> None:
-        """Advance the `pos` pointer and set the `current_char` variable."""
         self.pos += 1
-        if self.pos > len(self.text) - 1:
-            self.current_char = None  # Indicates end of input
-        else:
-            self.current_char = self.text[self.pos]
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
     def skip_whitespace(self) -> None:
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
     def integer(self) -> int:
-        """Return a (multidigit) integer consumed from the input."""
         result = ''
         while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
@@ -55,66 +40,53 @@ class Lexer(object):
         return int(result)
 
     def get_next_token(self) -> Token:
-        """Lexical analyzer (also known as scanner or tokenizer)
-
-        This method is responsible for breaking a sentence
-        apart into tokens. One token at a time.
-        """
         while self.current_char is not None:
-
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
-
             if self.current_char.isdigit():
                 return Token(INTEGER, self.integer())
-
             if self.current_char == '+':
                 self.advance()
                 return Token(PLUS, '+')
-
             if self.current_char == '-':
                 self.advance()
                 return Token(MINUS, '-')
-
             if self.current_char == '*':
                 self.advance()
                 return Token(MUL, '*')
-
             if self.current_char == '/':
                 self.advance()
                 return Token(DIV, '/')
-
             if self.current_char == '(':
                 self.advance()
                 return Token(LPAREN, '(')
-
             if self.current_char == ')':
                 self.advance()
                 return Token(RPAREN, ')')
-
             self.error()
-
         return Token(EOF, None)
 
-class AST(object):
+class AST:
     pass
-
 
 class BinOp(AST):
     def __init__(self, left: AST, op: Token, right: AST) -> None:
         self.left = left
-        self.token = self.op = op
+        self.op = op
         self.right = right
-
 
 class Num(AST):
     def __init__(self, token: Token) -> None:
         self.token = token
         self.value = token.value
 
+class UnaryOp(AST):
+    def __init__(self, op: Token, expr: AST) -> None:
+        self.op = op
+        self.expr = expr
 
-class Parser(object):
+class Parser:
     def __init__(self, lexer: Lexer) -> None:
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
@@ -129,9 +101,14 @@ class Parser(object):
             self.error()
 
     def factor(self) -> AST:
-        """factor : INTEGER | LPAREN expr RPAREN"""
         token = self.current_token
-        if token.type == INTEGER:
+        if token.type == PLUS:
+            self.eat(PLUS)
+            return UnaryOp(token, self.factor())
+        elif token.type == MINUS:
+            self.eat(MINUS)
+            return UnaryOp(token, self.factor())
+        elif token.type == INTEGER:
             self.eat(INTEGER)
             return Num(token)
         elif token.type == LPAREN:
@@ -141,43 +118,34 @@ class Parser(object):
             return node
 
     def term(self) -> AST:
-        """term : factor ((MUL | DIV) factor)*"""
         node = self.factor()
-
         while self.current_token.type in (MUL, DIV):
             token = self.current_token
             if token.type == MUL:
                 self.eat(MUL)
             elif token.type == DIV:
                 self.eat(DIV)
-
             node = BinOp(left=node, op=token, right=self.factor())
-
         return node
 
     def expr(self) -> AST:
-        """
-        expr   : term ((PLUS | MINUS) term)*
-        term   : factor ((MUL | DIV) factor)*
-        factor : INTEGER | LPAREN expr RPAREN
-        """
         node = self.term()
-
         while self.current_token.type in (PLUS, MINUS):
             token = self.current_token
             if token.type == PLUS:
                 self.eat(PLUS)
             elif token.type == MINUS:
                 self.eat(MINUS)
-
             node = BinOp(left=node, op=token, right=self.term())
-
         return node
 
     def parse(self) -> AST:
-        return self.expr()
+        node = self.expr()
+        if self.current_token.type != EOF:
+            self.error()
+        return node
 
-class NodeVisitor(object):
+class NodeVisitor:
     def visit(self, node: AST) -> any:
         method_name = 'visit_' + type(node).__name__
         visitor = getattr(self, method_name, self.generic_visit)
@@ -186,12 +154,11 @@ class NodeVisitor(object):
     def generic_visit(self, node: AST) -> None:
         raise Exception('No visit_{} method'.format(type(node).__name__))
 
-
 class Interpreter(NodeVisitor):
     def __init__(self, parser: Parser) -> None:
         self.parser = parser
 
-    def visit_BinOp(self, node: BinOp) -> float:
+    def visit_BinOp(self, node: BinOp) -> int:
         if node.op.type == PLUS:
             return self.visit(node.left) + self.visit(node.right)
         elif node.op.type == MINUS:
@@ -199,34 +166,35 @@ class Interpreter(NodeVisitor):
         elif node.op.type == MUL:
             return self.visit(node.left) * self.visit(node.right)
         elif node.op.type == DIV:
-            return self.visit(node.left) / self.visit(node.right)
+            return self.visit(node.left) // self.visit(node.right)
 
     def visit_Num(self, node: Num) -> int:
         return node.value
 
-    def interpret(self) -> float:
+    def visit_UnaryOp(self, node: UnaryOp) -> int:
+        op = node.op.type
+        if op == PLUS:
+            return +self.visit(node.expr)
+        elif op == MINUS:
+            return -self.visit(node.expr)
+
+    def interpret(self) -> int:
         tree = self.parser.parse()
         return self.visit(tree)
-
 
 def main() -> None:
     while True:
         try:
-            try:
-                text = raw_input('spi> ')
-            except NameError:  # Python3
-                text = input('spi> ')
+            text = input('spi> ')
         except EOFError:
             break
         if not text:
             continue
-
         lexer = Lexer(text)
         parser = Parser(lexer)
         interpreter = Interpreter(parser)
         result = interpreter.interpret()
         print(result)
-
 
 if __name__ == '__main__':
     main()
